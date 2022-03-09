0875554426fea0e95c7ce2c4903b0adf2f2aa6ad
ibenchi <horizen27@gmail.com>
2022-03-09 23:25:12 +0000

    storage api

--- options.js
+++ options.js
@@ -2,5 +2,16 @@ const nameInput = document.getElementById("name-input")
 const saveBtn = document.getElementById("save-btn")
 
 saveBtn.addEventListener("click", () => {
-  console.log(nameInput.value)
+  const name = nameInput.value
+  chrome.storage.sync.set(
+    {
+      name,
+    },
+    () => {
+      console.log("name is set")
+    }
+  )
+})
+chrome.storage.sync.get(["name", "test"], (res) => {
+  nameInput.value = res.name
 })
