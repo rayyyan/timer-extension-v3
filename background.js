3b9bde88e0a522ef1deb59ed6e5fe90028189e74
ibenchi <horizen27@gmail.com>
2022-03-10 07:28:18 +0000

    adding interval input in options page

--- background.js
+++ background.js
@@ -5,6 +5,7 @@ chrome.alarms.create({
 chrome.alarms.onAlarm.addListener((alarm) => {
   chrome.storage.local.get(["timer"], (res) => {
     const time = res.timer ?? 0
+
     chrome.storage.local.set({
       timer: time + 1,
     })
@@ -12,11 +13,15 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.action.setBadgeText({
       text: `${time + 1}`,
     })
-    if (time % 1000 == 0) {
-      this.registration.showNotification("Chrome Timer Extensions", {
-        body: "1 second passed",
-        icon: "icons.png",
-      })
-    }
+
+    chrome.storage.sync.get(["interval"], (res) => {
+      const interval = res.interval ?? 1000
+      if (time % interval == 0) {
+        this.registration.showNotification("Chrome Timer Extensions", {
+          body: `${interval} seconds has passed`,
+          icon: "icons.png",
+        })
+      }
+    })
   })
 })
--- options.js
+++ options.js
@@ -1,17 +1,20 @@
 const nameInput = document.getElementById("name-input")
 const saveBtn = document.getElementById("save-btn")
-
+const intervalInput = document.getElementById("interval-input")
 saveBtn.addEventListener("click", () => {
   const name = nameInput.value
+  const interval = intervalInput.value
   chrome.storage.sync.set(
     {
       name,
+      interval,
     },
     () => {
-      console.log("name is set")
+      console.log("name and interval is set")
     }
   )
 })
-chrome.storage.sync.get(["name"], (res) => {
-  nameInput.value = res.name
+chrome.storage.sync.get(["name", "interval"], (res) => {
+  nameInput.value = res.name ?? 0
+  intervalInput.value = res.interval ?? 1000
 })
